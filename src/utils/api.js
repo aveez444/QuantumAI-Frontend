d098a6c4b4acf81b2174dc63cdffd4a0f6942e02
aveez444 <aveezsiddiqui656@gmail.com>
2025-09-19 23:41:37 +0530

    comnn

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,10 @@
 // utils/api.js
 import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: API_BASE,
   headers: {
     'Content-Type': 'application/json',
     'X-Tenant-ID': '1',
